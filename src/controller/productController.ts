fb12be85697357b93b7484e292b66aec2483f28d
wagngomes <wagner.gomes.cx2@gmail.com>
2025-02-23 22:29:53 -0300

    feat: controller saldos

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -3,7 +3,8 @@ import { Request, Response } from "express";
 
 class ProductController {
   static async limpaBanco(req: Request, res: Response) {
-    await client.product.deleteMany();
+
+    await client.product.deleteMany()
     res.status(200).json("dados deletados com sucesso");
   }
 }
