35794ddacab2b14e7901bb637f4f8c3d60fdcffd
wagngomes <wagner.gomes.cx2@gmail.com>
2025-03-25 14:08:36 -0300

    feat: refact

--- src/controller/saldosUploadController.ts
+++ src/controller/saldosUploadController.ts
@@ -4,7 +4,7 @@ import multer from "multer";
 import storage from "../lib/multerConfig";
 import csv from "csv";
 import { client } from "../database/client";
-import { max, mean, min, sum } from "mathjs";
+import { SaldoInterface, saldosParse } from "../utils/csvParse";
 
 const upload: multer.Multer = multer({ storage: storage });
 
@@ -24,54 +24,23 @@ class SaldosUpLoadController {
       }
 
       const arquivoImportado = req.file.path;
-      //console.log("Arquivo recebido:", arquivoImportado)
 
       const parser = fs
         .createReadStream(arquivoImportado)
         .pipe(csv.parse({ columns: true, delimiter: ";" }));
 
-      const dadosArray = [];
+      const dadosArray: SaldoInterface[] = [];
 
       for await (const row of parser) {
-        const dadosTratados = {
-          codigo: row.codigo,
-          filial: row.filial,
-          id: row.codigo + row.filial,
-          rota: row.rota,
-          m4: Number(row.m4 !== undefined ? row.m4 : 0),
-          m3: Number(row.m3 !== undefined ? row.m3 : 0),
-          m2: Number(row.m2 !== undefined ? row.m2 : 0),
-          m1: Number(row.m1 !== undefined ? row.m1 : 0),
-          forecast: Number(row.forecast !== undefined ? row.forecast : 0),
-          estoque_in: Number(row.estoque_in !== undefined ? row.estoque_in : 0),
-          estoque_livre: Number(
-            row.estoque_livre !== undefined ? row.estoque_livre : 0
-          ),
-          compras: Number(row.compras !== undefined ? row.compras : 0),
-          transferencias: Number(
-            row.transferencias !== undefined ? row.transferencias : 0
-          ),
-          estoque_total: Number(
-            row.estoque_total !== undefined ? row.estoque_total : 0
-          ),
-          cmv: Number(row.cmv !== undefined ? row.cmv : 0),
-          mediaSimples: Number((row.m1 + row.m2 + row.m3) / 3),
-          ultimoMes: Number(row.m1),
-          novaMedia: Number((row.forecast + row.m1 + row.m2) / 3),
-          menorVenda: Number(min([row.m1, row.m2, row.m3, row.m4])),
-          maiorVenda: Number(max([row.m1, row.m2, row.m3, row.m4])),
-          mediaPonderada: Number(
-            row.m1 * 0.4 + row.m2 * 0.3 + row.m3 * 0.2 + row.m4 * 0.1
-          ),
-        };
 
-        dadosArray.push(dadosTratados);
+        const linhaDeSaldo = saldosParse(row)
+
+        dadosArray.push(linhaDeSaldo);
       }
 
       if (dadosArray.length > 0) {
         await client.inventory.createMany({ data: dadosArray });
       }
-
       res.status(200).send("Upload e processamento concluídos com sucesso.");
       fs.unlinkSync(arquivoImportado);
       return;
---
+++ src/utils/csvParse.ts
@@ -0,0 +1,53 @@
+import { max, min } from "mathjs"
+
+
+export interface SaldoInterface {
+    codigo: string
+    filial: string
+    id: string
+    rota: string
+    m4: number
+    m3: number
+    m2: number
+    m1: number
+    forecast: number
+    estoque_in: number
+    estoque_livre: number
+    compras: number
+    transferencias: number
+    estoque_total: number
+    cmv: number
+    mediaSimples: number
+    ultimoMes: number
+    novaMedia: number
+    menorVenda: number
+    maiorVenda: number
+    mediaPonderada: number
+}
+
+export function saldosParse (row: SaldoInterface){
+    let linhaDeSaldo = {
+    codigo: row.codigo,
+    filial: row.filial,
+    id: row.codigo + row.filial,
+    rota: row.rota,
+    m4: Number(row.m4 !== undefined ? row.m4 : 0),
+    m3: Number(row.m3 !== undefined ? row.m3 : 0),
+    m2: Number(row.m2 !== undefined ? row.m2 : 0),
+    m1: Number(row.m1 !== undefined ? row.m1 : 0),
+    forecast: Number(row.forecast !== undefined ? row.forecast : 0),
+    estoque_in: Number(row.estoque_in !== undefined ? row.estoque_in : 0),
+    estoque_livre: Number(row.estoque_livre !== undefined ? row.estoque_livre : 0),
+    compras: Number(row.compras !== undefined ? row.compras : 0),
+    transferencias: Number(row.transferencias !== undefined ? row.transferencias : 0),
+    estoque_total: Number(row.estoque_total !== undefined ? row.estoque_total : 0),
+    cmv: Number(row.cmv !== undefined ? row.cmv : 0),
+    mediaSimples: Number((row.m1 + row.m2 + row.m3) / 3),
+    ultimoMes: Number(row.m1),
+    novaMedia: Number((row.forecast + row.m1 + row.m2) / 3),
+    menorVenda: Number(row.m1 || 0),
+    maiorVenda: Number(row.m1 || 0),
+    mediaPonderada: Number(row.m1 * 0.4 + row.m2 * 0.3 + row.m3 * 0.2 + row.m4 * 0.1)
+    }
+    return  linhaDeSaldo
+}
