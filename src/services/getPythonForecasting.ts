5bb8984259d5966a037b6b98abc8e258b55b7b6a
wagngomes <wagner.gomes.cx2@gmail.com>
2025-04-22 14:03:25 -0300

    feat:

--- src/services/getPythonForecasting.ts
+++ src/services/getPythonForecasting.ts
@@ -1,7 +1,6 @@
 export class GetPythonForecasting {
     constructor(private HistoricRepository: any){
     }
-
     async execute(){
 
         const historic = await this.HistoricRepository.getHistoricData()
@@ -17,20 +16,17 @@ export class GetPythonForecasting {
             cd: item.cd,
             codigo_produto: item.product
           }))
-
         const response = await fetch ('http://localhost:8000/forecast',{
             method: 'POST',
             headers:{
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({items:formattedData})
-
         })
         if (!response.ok) {
             throw new Error(`Erro ao chamar o forecasting: ${response.status}`)
         }
         const data = await response.json()
-        return data
-       
+        return data       
     }
 }
